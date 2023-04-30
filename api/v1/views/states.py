a257a4e2bb53901d7f0fe75595df917ff9037e18
Jerry Wonder <jerrycul2001@gmail.com>
2023-04-30 19:38:12 +0100

    feat: add endpoint to get a state by id

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,9 +4,18 @@
 from api.v1.views import app_views
 from models.state import State
 from models import storage
+from flask import abort
 
 
 @app_views.route('/states')
 def get_all_states():
     obj = storage.all(State)
     return [v.to_dict() for v in obj.values()]
+
+
+@app_views.route('/states/<state_id>')
+def get_state_by_id(state_id):
+    obj = storage.get(State, state_id)
+    if (obj != None):
+        return obj.to_dict()
+    return abort(404)
