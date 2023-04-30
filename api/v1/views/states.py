261f0145480cbc0a4df4092da79a3b0a237662c4
Jerry Wonder <jerrycul2001@gmail.com>
2023-04-30 19:39:06 +0100

    chore: add docs for route handlers

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,12 +9,14 @@ from flask import abort
 
 @app_views.route('/states')
 def get_all_states():
+    """get all states"""
     obj = storage.all(State)
     return [v.to_dict() for v in obj.values()]
 
 
 @app_views.route('/states/<state_id>')
 def get_state_by_id(state_id):
+    """get a state by id"""
     obj = storage.get(State, state_id)
     if (obj != None):
         return obj.to_dict()
