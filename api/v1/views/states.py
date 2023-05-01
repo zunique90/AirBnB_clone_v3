e35fe1afb5e78e3f05451dda871fa7ca9f9871ce
charles ezebuike <ezebuike.charles0@gmail.com>
2023-05-01 21:18:10 +0000

    return obj.to_json

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -11,36 +11,25 @@ cls = State
 IGNORE_LIST = ['id', 'updated_at', 'created_at']
 
 
-def clean(attr_dict, obj):
-    """Just makes sure that keys from IGNORE_LIST are not being set"""
-    return {k: v for k, v in attr_dict.items() if k not in IGNORE_LIST}
-
-
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_all():
     """Get all states"""
     obj = storage.all(cls)
-    parsed = [v.to_dict() for v in obj.values()]
-    return jsonify(parsed), 200
+    parsed = [v.to_json() for v in obj.values()]
+    return jsonify(parsed)
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create():
     """create a new state"""
-    try:
-        body = request.get_json()
-        body = clean(body)
-        if type(body) is not dict:
-            raise BadRequest()
-        if 'name' not in body:
-            raise KeyError()
-        obj = cls(**body)
-        obj.save()
-        return jsonify(obj.to_dict()), 201
-    except BadRequest as e:
-        abort(400, 'Not a JSON')
-    except KeyError as e:
-        abort(400, 'Missing name')
+    body = request.get_json()
+    if body is None:
+        abort(400, "Not a JSON")
+    if body.get("name") is None:
+        abort(400 "Missing name")
+    obj = cls(**body)
+    obj.save()
+    return jsonify(obj.to_json()), 201
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
@@ -48,8 +37,8 @@ def get(state_id):
     """Get a state by ID"""
     obj = storage.get(cls, state_id)
     if obj is None:
-        return abort(404)
-    return jsonify(obj.to_dict()), 200
+        abort(404, "Not found")
+    return jsonify(obj.to_json())
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'],
@@ -58,10 +47,10 @@ def delete(state_id):
     """Delete a state"""
     obj = storage.get(cls, state_id)
     if obj is None:
-        return abort(404)
-    storage.delete(obj)
-    storage.save()
-    return jsonify({}), 200
+        abort(404, "Not found")
+    obj.delete()
+    del obj
+    return jsonify({})
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
@@ -69,16 +58,8 @@ def update(state_id):
     """Update a state"""
     obj = storage.get(cls, state_id)
     if obj is None:
-        return abort(404)
-    try:
-        body = request.get_json()
-        body = clean(body)
-        for key, value in body.items():
-            print(key in IGNORE_LIST)
-            if key in IGNORE_LIST:
-                continue
-            setattr(obj, key, value)
-        obj.save()
-    except BadRequest as e:
-        abort(400, 'Not a JSON')
-    return jsonify(obj.to_dict()), 200
+        abort(404, "Not found")
+    body = request.get_json()
+    if body is None:
+        abort(400, "Not a JSON")
+    return jsonify(obj.to_json())
